d0d5d6ebff3caab05f39fc60a92a92ab9b0b2ec7
pablo-psichalis <pablo.psrz@gmail.com>
2017-09-10 14:59:40 +0200

    Refactorizaciones varias y cambios menores.

--- src/Damas.java
+++ src/Damas.java
@@ -26,7 +26,9 @@ public class Damas {
 
         int i = 0;
 
+        System.out.println("  A B C D E F G H");
         for (int row = 0; row < Board.NUM_ROWS; row++) {
+            System.out.print(row+1);
             if (row % 2 == 0) {
                 for (int col = 0; col < Board.NUM_COLUMNS; col += 2, i++) {
                     System.out.print("| ");
--- src/Token/TokenBlack.java
+++ src/Token/TokenBlack.java
@@ -36,43 +36,44 @@ public class TokenBlack extends Token {
         int possibleDestination
                 = (this.getCurrentSquareIdentifier() + BLACK.getOffsetForwardLeft()) % Board.MAX_SQUARE_ID;
 
-        if (!board.isOccupied(possibleDestination)) {
-            return true;
-        } else if (board.isOccupiedByBlack(possibleDestination)) {
-            return false;
-        } else {
-            assert board.isOccupiedByRed(possibleDestination);
-            return (
-                    !board.isOccupied(
-                            (possibleDestination + BLACK.getOffsetForwardLeft()) % Board.MAX_SQUARE_ID
-                    )
-            );
-        }
+        return (!board.isOccupied(possibleDestination));
+    }
+
+    public boolean canCaptureTokenForwardLeft(Board board) {
+
+        int possibleDestination
+                = (this.getCurrentSquareIdentifier() + BLACK.getOffsetForwardLeft()) % Board.MAX_SQUARE_ID;
+
+        assert board.isOccupiedByRed(possibleDestination);
+        return (
+                !board.isOccupied(
+                        (possibleDestination + BLACK.getOffsetForwardLeft()) % Board.MAX_SQUARE_ID
+                )
+        );
     }
 
     public boolean canMoveForwardRight(Board board) {
+
         int possibleDestination
                 = (this.getCurrentSquareIdentifier() + BLACK.getOffsetForwardRight()) % Board.MAX_SQUARE_ID;
 
-        if (!board.isOccupied(possibleDestination)) {
-            return true;
-        } else if (board.isOccupiedByBlack(possibleDestination)) {
-            return false;
-        } else {
-            assert board.isOccupiedByRed(possibleDestination);
-            return (
-                    !board.isOccupied(
-                            (possibleDestination + BLACK.getOffsetForwardRight()) % Board.MAX_SQUARE_ID
-                    )
-            );
-        }
+        return (!board.isOccupied(possibleDestination));
+    }
+
+    public boolean canCaptureTokenForwardRight(Board board) {
+
+        int possibleDestination
+                = (this.getCurrentSquareIdentifier() + BLACK.getOffsetForwardRight()) % Board.MAX_SQUARE_ID;
+
+        assert board.isOccupiedByRed(possibleDestination);
+        return (
+                !board.isOccupied(
+                        (possibleDestination + BLACK.getOffsetForwardRight()) % Board.MAX_SQUARE_ID
+                )
+        );
     }
 
     public boolean canCapture(int tokenSquareId, Board board) {
-        if(board.getOccupiedByRed().containsKey(tokenSquareId)){
-            return true;
-        } else {
-            return false;
-        }
+        return (board.getOccupiedByRed().containsKey(tokenSquareId));
     }
 }
